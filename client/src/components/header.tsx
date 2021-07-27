a4a68f756904ecba1d8e1adebd3d84c229e7ed77
firecoded <yepyepguru@gmail.com>
2021-07-26 20:52:11 -0700

    add project, update favicon, move readme, improve styling and responsive design

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -3,14 +3,15 @@ import React from "react";
 export const Header = (): JSX.Element => {
     return (
         <h1 className="main-header d-flex flex-column">
-            <span>
-                Jacob Taylor<span className="highlight-text">,</span>
+            <span className="larger-text">
+                Jacob <br />
+                Taylor<span className="highlight-text">,</span>
             </span>
             <span>
                 Full Stack<span className="highlight-text">,</span>
             </span>
             <span>
-                UI<span className="highlight-text">/</span> UX Advocate<span className="highlight-text">,</span>
+                UI<span className="highlight-text">/</span>UX Advocate<span className="highlight-text">,</span>
             </span>
             <span>
                 Software Engineer<span className="highlight-text">.</span>
--- client/src/components/projects.tsx
+++ client/src/components/projects.tsx
@@ -1,10 +1,21 @@
-import msweep from "../assets/imgs/msweep.png";
+import msweep from "../assets/imgs/msweep-lg.png";
+import devhub from "../assets/imgs/devhub-lg.png";
 
 export const Projects = (): JSX.Element => {
     const carouselData = [
         {
+            imgSrc: devhub,
+            imgAlt: "DevHub app image",
+            cardTitle: "DevHub",
+            cardBody:
+                "Full-stack social media application for developers to connect, create profiles, add/like posts, and discuss posts further. Fun project to work on in my limited free time, recently added a theme toggle.",
+            techUsed: "ReactJS, Redux, SCSS, Node, Express, MongoDB, JWT",
+            github: "https://github.com/Firecoded/dev_connect",
+            liveDemo: "https://salty-caverns-71357.herokuapp.com/",
+        },
+        {
             imgSrc: msweep,
-            imgAlt: "Minesweeper Game Image",
+            imgAlt: "Minesweeper game image",
             cardTitle: "MinesweeperJS",
             cardBody:
                 "Building simple games often times presents complex problems. The task is like a series of practice problems that ends with game to show off in the end. Using a framework like ReactJS to build games requires some creative logic as opposed to more traditional frameworks",
@@ -12,33 +23,36 @@ export const Projects = (): JSX.Element => {
             github: "https://github.com/Firecoded/msweep",
             liveDemo: "https://hardcore-saha-279f59.netlify.app/",
         },
-        {
-            imgSrc: "...",
-            imgAlt: "",
-            cardTitle: "MinesweeperJS",
-            cardBody: "This is some temporary text",
-            techUsed: "",
-            github: "https://github.com/Firecoded/msweep",
-            liveDemo: "https://hardcore-saha-279f59.netlify.app/",
-        },
     ];
     const buildCarouselItems = () => {
         return carouselData.map((c, i) => {
             return (
-                <div className={`carousel-item ${i === 0 ? "active" : ""} h-100`} data-interval={false} key={i}>
+                <div className={`carousel-item ${i === 0 ? "active" : ""} h-100`} data-interval="false" key={i}>
                     <div className="d-flex justify-content-center align-items-center h-100">
                         <div className="card mb-3">
                             <img src={c.imgSrc} className="card-img-top" alt={c.imgAlt} />
                             <div className="card-body">
-                                <h5 className="card-title">{c.cardTitle}</h5>
+                                <h4 className="card-title">
+                                    <strong>{c.cardTitle}</strong>
+                                </h4>
                                 <p className="card-text">{c.cardBody}</p>
                                 <p className="card-text">
                                     <span className="text-muted">Tech Used: {c.techUsed}</span>
                                 </p>
-                                <a href={c.github} className="card-link" target="_blank" rel="noreferrer">
+                                <a
+                                    href={c.github}
+                                    className="card-link highlight-text"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
                                     GitHub Repo
                                 </a>
-                                <a href={c.liveDemo} className="card-link" target="_blank" rel="noreferrer">
+                                <a
+                                    href={c.liveDemo}
+                                    className="card-link highlight-text"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
                                     Live Demo
                                 </a>
                             </div>
@@ -49,7 +63,12 @@ export const Projects = (): JSX.Element => {
         });
     };
     return (
-        <div id="carouselIndicators" className="carousel slide position-relative h-100 w-100" data-ride="carousel">
+        <div
+            id="carouselIndicators"
+            className="carousel slide position-relative h-100 w-100"
+            data-ride="carousel"
+            data-interval="false"
+        >
             <div className="d-flex w-50 justify-content-center">
                 <ol className="carousel-indicators">
                     {carouselData.map((c, i) => {
